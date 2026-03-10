a0687ba31cd0bdf4ee20cc1fabeaab3baafd9746
prathamkhairmode15 <prathamkhairmode15@gmail.com>
2026-03-10 12:22:23 +0530

    improved login

--- login/frontend/src/lib/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = "https://saecwjmbowqxkgeznyjb.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_XuzTy21DGcMJCFpjcMIXrg_MetI0Pac";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
